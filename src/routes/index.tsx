0e6ffda18812a7edd462c2f6f2dade917822025d
has-bii <hasbii.rofficial@gmail.com>
2026-05-08 01:13:03 +0700

    feat!(Home): remove Nav Component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,6 @@
 import AboutMe from '@/components/home/AboutMe'
 import CTA from '@/components/home/CTA'
 import Hero from '@/components/home/Hero'
-import NavBar from '@/components/home/NavBar'
 import SelectedWork from '@/components/home/SelectedWork'
 import Services from '@/components/home/Services'
 import Footer from '@/components/Footer'
@@ -13,7 +12,6 @@ export const Route = createFileRoute('/')({ component: Home })
 function Home() {
   return (
     <>
-      <NavBar />
       <Hero />
       <Separator />
       <AboutMe />
