3d1059d2a037cb6063ab2fb6769884137d931da0
has-bii <hasbii.rofficial@gmail.com>
2026-05-08 01:09:57 +0700

    feat(social): update social links

--- src/data/socials.ts
+++ src/data/socials.ts
@@ -1,8 +1,8 @@
 const socials = [
-  { label: 'GitHub', href: 'https://github.com/hasbii', icon: 'Github' },
-  { label: 'LinkedIn', href: 'https://linkedin.com/in/hasbii', icon: 'Linkedin' },
-  { label: 'Instagram', href: 'https://instagram.com/hasbii', icon: 'Instagram' },
-  { label: 'Email', href: 'mailto:hello@hasbii.dev', icon: 'Mail' },
+  { label: 'GitHub', href: 'https://github.com/has-bii', icon: 'Github' },
+  { label: 'LinkedIn', href: 'https://linkedin.com/in/hasbiy-robbiy', icon: 'Linkedin' },
+  { label: 'Instagram', href: 'https://instagram.com/has.bii', icon: 'Instagram' },
+  { label: 'Email', href: 'mailto:hasbii.rofficial@gmail.com', icon: 'Mail' },
 ] as const
 
 export default socials
